30cedeb51f9e4c52a29620240c49db4854045854
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2025-08-25 16:40:52 +0600

    adjusted tooltip on cost estimator page

--- src/pages/UserDashboard/CostEstimator.tsx
+++ src/pages/UserDashboard/CostEstimator.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { HelpCircle } from "lucide-react";
 
 const CostEstimator: React.FC = () => {
+  const [showHelp, setShowHelp] = useState(false);
   const currencies = [
     { symbol: "A$", code: "AUD" },
     { symbol: "$", code: "USD" },
@@ -23,18 +24,39 @@ const CostEstimator: React.FC = () => {
       </div>
 
       {/* Info Note */}
-      <div className="flex flex-col md:flex-row items-start md:justify-between mt-6 bg-[#0B1739] text-gray-300 px-4 sm:px-5 py-3 rounded-md">
-        <p>
+      <div className="flex md:max-w-5xl md:flex-row items-start md:justify-between mt-6 bg-[#0B1739] text-gray-300 px-4 sm:px-5 py-3 rounded-md">
+        <p className="text-sm">
           By the way – You’re not creating a campaign here. This is purely to
           help you get a ballpark figure of budget required for a given
           selection of boards.
         </p>
-        <HelpCircle className="ml-0 md:ml-3 mt-1 w-5 h-5 text-gray-400 cursor-pointer hover:text-[#fff] shrink-0" />
+        {/* Help Button */}
+        <div
+          className=""
+          onMouseEnter={() => setShowHelp(true)}
+          onMouseLeave={() => setShowHelp(false)}
+        >
+          <HelpCircle className="ml-0 md:ml-3 mt-1 w-5 h-5 text-gray-400 cursor-pointer hover:text-white shrink-0" />
+
+          {/* Tooltip */}
+          {showHelp && (
+            <div className="absolute top-65 right-70 md:max-w-md bg-[#0B1739] rounded-md p-6 text-gray-300 shadow-lg z-20">
+              <h3 className="font-semibold mb-5 text-white">
+                The cost estimate here…is just an estimate.
+              </h3>
+              <p className="text-sm leading-relaxed">
+                As you can imagine, the cost of an ad on a board depends on a
+                bunch of factors – like time of day, location, impact, and so
+                on. To make things faster, the cost estimator pretty much
+                ignores all of that.
+              </p>
+            </div>
+          )}
+        </div>
       </div>
 
-      {/* Right Side Note */}
-      <div className="flex flex-col md:flex-row justify-between mt-10 gap-8 md:gap-6">
-        {/* Step One */}
+      {/* Step One */}
+      <div className="flex flex-col md:flex-row justify-between mt-5 gap-8 md:gap-6">
         <div className="mt-8 md:md-12">
           <h2 className="text-xl font-bold mb-4">Step One :</h2>
           <p className="text-[#fff] text-xl mb-6">Pick your currency</p>
@@ -49,16 +71,6 @@ const CostEstimator: React.FC = () => {
               </button>
             ))}
           </div>
-        </div>
-        <div className="md:max-w-sm max-w-full bg-[#0B1739] rounded-md p-6 text-gray-300">
-          <h3 className="font-semibold mb-5 text-white">
-            The cost estimate here…is just an estimate.
-          </h3>
-          <p className="text-sm leading-relaxed">
-            As you can imagine, the cost of an ad on a board depends on a bunch
-            of factors – like time of day, location, impact, and so on. To make
-            things faster, the cost estimator pretty much ignores all of that.
-          </p>
         </div>
       </div>
     </div>
