6a80ff3db84b0b837c072b939a3bce6a99576a5b
Joyeta-Mondal <dipannitakotha2019@gmail.com>
2025-08-27 08:46:51 +0600

    adjusted padding of profile of admin dashboard

--- src/pages/AdminDashboard/AdminCampaignData.tsx
+++ src/pages/AdminDashboard/AdminCampaignData.tsx
@@ -145,41 +145,41 @@ const AdminCampaignData: React.FC = () => {
 
               {/* Profile section */}
 
-              <div >
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button
-                    variant="ghost"
-                    className="flex items-center gap-2 text-white px-4 py-3 focus:ring-0 focus:outline-none focus:border-none border-none ring-0 rounded-full cursor-pointer transition-all duration-300"
-                  >
-                    <div className="w-8 h-8 bg-[linear-gradient(291deg,_#38B6FF_-45.64%,_#09489D_69.04%)] rounded-full flex items-center justify-center">
-                      <User className="h-4 w-4" />
-                    </div>
-                    <span className="hidden sm:inline font-medium">SS</span>
-                    <ChevronDown className="h-4 w-4" />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent
-                  align="end"
-                  className="w-full bg-[#1A2342] border-none flex items-center shadow-[0_0_12px_rgba(9,72,157,0.9)]  justify-center flex-col"
-                >
-                  <DropdownMenuItem className="cursor-pointer py-2 hover:bg-[linear-gradient(291deg,_#38B6FF_-45.64%,_#09489D_69.04%)] text-secondary-color hover:text-white px-5 w-full">
-                    <Link
-                      to="/admin-dashboard/adminBasicInfo"
-                      className="flex items-center w-full"
+              <div>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button
+                      variant="ghost"
+                      className="flex items-center gap-2 text-white px-4 py-3 focus:ring-0 focus:outline-none focus:border-none border-none ring-0 rounded-full cursor-pointer transition-all duration-300"
                     >
-                      <User className="h-4 w-4 mr-2" />
-                      Profile
-                    </Link>
-                  </DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem className="cursor-pointer py-1 flex items-center justify-between px-5 hover:bg-[linear-gradient(291deg,_#38B6FF_-45.64%,_#09489D_69.04%)] w-full text-red-600">
-                    <LogOut className="h-4 w-4 mr-2" />
-                    Logout
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
+                      <div className="w-8 h-8 bg-[linear-gradient(291deg,_#38B6FF_-45.64%,_#09489D_69.04%)] rounded-full flex items-center justify-center">
+                        <User className="h-4 w-4" />
+                      </div>
+                      <span className="hidden sm:inline font-medium">SS</span>
+                      <ChevronDown className="h-4 w-4" />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent
+                    align="end"
+                    className="w-full bg-[#1A2342] border-none flex items-center shadow-[0_0_12px_rgba(9,72,157,0.9)]  justify-center flex-col"
+                  >
+                    <DropdownMenuItem className="cursor-pointer py-2 hover:bg-[linear-gradient(291deg,_#38B6FF_-45.64%,_#09489D_69.04%)] text-secondary-color hover:text-white px-5 w-full">
+                      <Link
+                        to="/admin-dashboard/adminBasicInfo"
+                        className="flex items-center w-full"
+                      >
+                        <User className="h-4 w-4 mr-2" />
+                        Profile
+                      </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem className="cursor-pointer py-2 px-5 flex items-center justify-between hover:bg-[linear-gradient(291deg,_#38B6FF_-45.64%,_#09489D_69.04%)] w-full text-red-600">
+                      <LogOut className="h-4 w-4 mr-2" />
+                      Logout
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
             </div>
             <div className="sm:hidden flex items-center">
               <Menu className="w-7 h-7 text-white cursor-pointer" />
--- src/pages/AdminDashboard/AdminDashboardHomePage.tsx
+++ src/pages/AdminDashboard/AdminDashboardHomePage.tsx
@@ -49,7 +49,7 @@ const AdminDashboardHomePage = () => {
                   align="end"
                   className="w-full bg-[#1A2342] border-none flex items-center shadow-[0_0_12px_rgba(9,72,157,0.9)]  justify-center flex-col"
                 >
-                  <DropdownMenuItem className="cursor-pointer py-2 hover:bg-[linear-gradient(291deg,_#38B6FF_-45.64%,_#09489D_69.04%)] text-secondary-color hover:text-white px-5 w-full">
+                  <DropdownMenuItem className="cursor-pointer py-2 px-5 hover:bg-[linear-gradient(291deg,_#38B6FF_-45.64%,_#09489D_69.04%)] text-secondary-color hover:text-white  w-full">
                     <Link
                       to="/admin-dashboard/adminBasicInfo"
                       className="flex items-center w-full"
@@ -59,7 +59,7 @@ const AdminDashboardHomePage = () => {
                     </Link>
                   </DropdownMenuItem>
                   <DropdownMenuSeparator />
-                  <DropdownMenuItem className="cursor-pointer py-1 flex items-center justify-between px-5 hover:bg-[linear-gradient(291deg,_#38B6FF_-45.64%,_#09489D_69.04%)] w-full text-red-600">
+                  <DropdownMenuItem className="cursor-pointer py-2 px-5 flex items-center justify-between  hover:bg-[linear-gradient(291deg,_#38B6FF_-45.64%,_#09489D_69.04%)] w-full text-red-600">
                     <LogOut className="h-4 w-4 mr-2" />
                     Logout
                   </DropdownMenuItem>
